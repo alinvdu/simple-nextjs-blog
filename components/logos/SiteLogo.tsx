da81c842862b9a0b156f448d2aabea49df1e2bc2
bobergsatoko <boberg.satoko@gmail.com>
2024-05-12 18:08:24 +0200

    Small changes

--- components/logos/SiteLogo.tsx
+++ components/logos/SiteLogo.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import OBMLogo from './OBMLogo';
 import { GlobalData } from '../../lib/types';
+import PngLogo from "./alivemachineslogo.png";
 
 export default function SiteLogo({
   siteData,
@@ -9,11 +10,19 @@ export default function SiteLogo({
 }): JSX.Element {
   return (
     <div className="mx-auto flex w-full max-w-3xl flex-col items-center justify-between px-4 py-4 md:flex-row lg:px-0">
-      <h1 className="flex space-x-2">
-        <OBMLogo className="h-8 w-8" />
+      <h1 className="flex space-x-2" style={{
+        alignItems: 'center'
+      }}>
+        <img src={PngLogo.src} style={{
+          width: 50
+        }} />
         <Link
           href="/"
-          className="bg-gradient-to-r from-cyan-700 to-teal-600 bg-clip-text text-4xl font-bold tracking-tighter text-transparent dark:from-cyan-300 dark:to-teal-200"
+          className="bg-gradient-to-r bg-clip-text text-3xl font-bold tracking-tighter text-zinc-500 dark:text-zinc-200"
+          style={{
+            fontWeight: 200,
+            letterSpacing: 0.3
+          }}
         >
           {siteData.metadata.site_title}
         </Link>
