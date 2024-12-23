f3e2d961a3596993148353657225acf57427a134
Alin Vasile Dumitru <dumitru.alin25@gmail.com>
2024-12-23 12:36:50 +0100

    Update page.tsx

--- app/author/[slug]/page.tsx
+++ app/author/[slug]/page.tsx
@@ -18,7 +18,7 @@ export default async ({ params }: { params: { id: string; slug: string } }) => {
   const posts = await getAuthorPosts({ authorId: author.id });
 
   return (
-    <main className="mx-auto w-full max-w-3xl flex-col px-4 lg:px-0">
+    <main className="mx-auto w-full max-w-5xl flex-col px-4 lg:px-0">
       <h1 className="my-4 text-4xl font-bold leading-tight tracking-tight text-zinc-700 dark:text-zinc-300">
         Posts by {author.title}
       </h1>
