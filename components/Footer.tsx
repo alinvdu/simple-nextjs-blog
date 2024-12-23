151f2427909b053a9b84b57d5fbbaf1aa2492700
Alin Vasile Dumitru <dumitru.alin25@gmail.com>
2024-12-23 02:16:53 +0100

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import CosmicLogo from './logos/CosmicLogo';
 
 export default function Footer(): JSX.Element {
   return (
-    <footer className="mx-auto mt-8 flex w-full max-w-3xl items-center justify-between px-4 py-4 text-xs md:text-sm lg:px-0 lg:text-base">
+    <footer className="mx-auto mt-8 flex w-full max-w-5xl items-center justify-between px-4 py-4 text-xs md:text-sm lg:px-0 lg:text-base">
       <a
         href="https://www.cosmicjs.com"
         target="_blank"
