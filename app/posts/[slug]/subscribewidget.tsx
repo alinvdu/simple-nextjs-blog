b12507bada593ce3d7a33c53a47e93471cb8b86e
Alin Vasile Dumitru <dumitru.alin25@gmail.com>
2024-12-25 13:12:16 +0100

    Update subscribewidget.tsx

--- app/posts/[slug]/subscribewidget.tsx
+++ app/posts/[slug]/subscribewidget.tsx
@@ -6,11 +6,10 @@ const SubscribeWidget = () => {
   const [showWidget, setShowWidget] = useState(false);
   const [permanentlyDismissed, setPermanentlyDismissed] = useState(false);
 
-  // Effect to handle showing the widget after scrolling down
   useEffect(() => {
     const handleScroll = () => {
-      const scrollPosition = window.scrollY; // Current scroll position
-      if (scrollPosition > 300 && !permanentlyDismissed) { // Show widget if not dismissed
+      const scrollPosition = window.scrollY;
+      if (scrollPosition > 300 && !permanentlyDismissed) {
         setShowWidget(true);
       }
     };
@@ -21,22 +20,22 @@ const SubscribeWidget = () => {
     };
   }, [permanentlyDismissed]);
 
-  // Function to hide the widget permanently
+  // Hide widget permanently
   const hideWidget = () => {
-    setPermanentlyDismissed(true); // Prevent widget from reappearing
-    setShowWidget(false); // Hide the widget
+    setPermanentlyDismissed(true);
+    setShowWidget(false);
   };
 
   return (
     <>
       {showWidget && (
         <div
-          className="fixed top-24 right-4 bg-black shadow-lg rounded-lg p-4 pr-8 flex flex-col space-y-2 dark:bg-zinc-800 relative"
+          className="fixed top-24 right-4 bg-black shadow-lg rounded-lg p-4 pr-6 flex flex-col space-y-2 dark:bg-zinc-800 relative"
           style={{ zIndex: 1000 }}
         >
           <button
             onClick={hideWidget}
-            className="absolute top-2 right-2 w-6 h-6 bg-white text-black rounded-full hover:bg-black hover:text-white flex items-center justify-center"
+            className="absolute top-1 right-1 w-5 h-5 bg-white text-black rounded-full hover:bg-black hover:text-white flex items-center justify-center"
           >
             ✕
           </button>
