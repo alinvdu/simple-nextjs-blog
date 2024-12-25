34ddd18a6046a7b61d8b2fd791cb0297a5a5e202
Alin Vasile Dumitru <dumitru.alin25@gmail.com>
2024-12-25 13:49:27 +0100

    Update subscribewidget.tsx

--- app/posts/[slug]/subscribewidget.tsx
+++ app/posts/[slug]/subscribewidget.tsx
@@ -10,7 +10,7 @@ const SubscribeWidget = () => {
   useEffect(() => {
     const handleScroll = () => {
       const scrollPosition = window.scrollY; // Current scroll position
-      if (scrollPosition > 300 && !permanentlyDismissed) { // Show widget if not dismissed
+      if (scrollPosition > 1500 && !permanentlyDismissed) { // Show widget if not dismissed
         setShowWidget(true);
       }
     };
