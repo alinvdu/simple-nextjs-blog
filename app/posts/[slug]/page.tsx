a900245a8a06127d4fb864ed6e59994e5889828b
Alin Vasile Dumitru <dumitru.alin25@gmail.com>
2024-12-25 18:54:21 +0100

    Update page.tsx

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -260,7 +260,7 @@ export default async ({ params }: { params: { slug: string } }) => {
         <Image
           width={1400}
           height={720}
-          className="mb-5 h-[720px] w-full bg-no-repeat object-cover object-center sm:object-contain"
+          className="mb-5 h-[720px] w-full bg-no-repeat object-cover object-contain"
           src={`${post.metadata.hero?.imgix_url}?w=1400&auto=format`}
           priority
           alt={post.title}
