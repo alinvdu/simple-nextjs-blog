afdc55ac832673716a8aee75bb3b61fa9bc95e7a
Alin Vasile Dumitru <dumitru.alin25@gmail.com>
2025-01-03 23:32:18 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Page(): Promise<JSX.Element> {
             </div>
           ))}
       </section>
-      <h1 className="text-2xl pb-[0px] font-bold text-white mb-2">Bits - Ideas & Pieces</h1>
+      <h1 className="text-2xl pb-[0px] font-bold text-white mb-2 mt-10">Bits - Ideas & Pieces</h1>
       <div className="pb-[20px] text-white">
         Small articles with fast written ideas and bits, containing references to papers, other reading material or videos.
       </div>
